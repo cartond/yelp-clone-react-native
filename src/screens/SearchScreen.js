47540c267651ce7adf371faf611b00a48263fc1b
Daniel Carton <cartond93@gmail.com>
2020-05-12 09:25:33 -0400

    Yelp search fest

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,33 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
 
-import SearchBar from '../components/SearchBar'
+import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [restaurants, setRestaurants] = useState([])
+
+  const searchApi = async() => {
+    const response = await yelp.get('/search', {
+      params:{
+        limit: 50,
+        term,
+        location: 'greenville'
+      }
+    });
+    setRestaurants(response.data.businesses);
+  }
 
   return (
     <View style={styles.background}>
       <SearchBar
         term={term}
-        onTermChange={(val) => setTerm(val)} 
-        onTermSubmit={()=>console.log('go fetch')}
+        onTermChange={setTerm} 
+        onTermSubmit={searchApi}
       />
       <Text>Searching for {term}</Text>
+      <Text>We have found {restaurants.length} restaurants</Text>
     </View>
   );
 };
